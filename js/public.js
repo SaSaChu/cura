690827a9e302241a7c7ff3ca7b22e180d92420fe
OA Wu <comdan66@gmail.com>
2018-01-14 14:45:16 +0800

    🤔 完成修改 Banner

--- js/public.js
+++ js/public.js
@@ -6,5 +6,19 @@
 
 $(function () {
 
+  $('._ic').imgLiquid ({ verticalAlign:'center' });
+  $('.banner_boxs').each (function () {
+    var $that = $(this);
+    var $pagination = $(this).find ('.pagination').empty ();
+
+    $pagination.append ($('<div />').append (Array.apply (null, Array ($(this).find ('>.bbox').length)).map (function (_t, i) {
+      return $('<a />').click (function () {
+        $(this).addClass ('a').siblings ().removeClass ('a');
+        $that.attr ('class', 'banner_boxs b' + $(this).index ());
+      });
+    })));
+
+    $pagination.find ('a').first ().click ();
+  });
   
 });
