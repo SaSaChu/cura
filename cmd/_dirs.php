aa78b760c6214df9856796b8e56228c936f5c489
OA Wu <comdan66@gmail.com>
2018-02-05 15:29:39 +0800

    修正部署條件。

--- cmd/_dirs.php
+++ cmd/_dirs.php
@@ -18,7 +18,8 @@ $_dirs = array (
   'tc'   => [['html'], true, false],
   'jp'   => [['html'], true, false],
   'en'   => [['html'], true, false],
-
+  
+  'js'   => [['js'], true, false],
   'css'  => [['css'], true, false],
   'font' => [['eot', 'svg', 'ttf', 'woff'], true, false],
   'img'  => [['png', 'jpg', 'jpeg', 'gif', 'svg'], true, false],
