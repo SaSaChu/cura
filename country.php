1e657297a513a15cb16cbe01cd33c4dc4d04ec3f
OA Wu <comdan66@gmail.com>
2018-02-05 15:15:24 +0800

    加入首頁判斷國家。

---
+++ country.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2018, OACI
+ * @link        https://www.ioa.tw/
+ */
+
+if (!function_exists ('validIp')) {
+  function validIp ($ip, $which = '') {
+    switch (strtolower ($which)) {
+      case 'ipv4':
+        $which = FILTER_FLAG_IPV4;
+        break;
+
+      case 'ipv6':
+        $which = FILTER_FLAG_IPV6;
+        break;
+
+      default:
+        $which = null;
+        break;
+    }
+
+    return (bool)filter_var ($ip, FILTER_VALIDATE_IP, $which);
+  }
+}
+if (!function_exists ('ip')) {
+  function ip () {
+    static $ip;
+    if ($ip !== null)
+      return $ip;
+
+    $ip = $_SERVER['REMOTE_ADDR'];
+
+    return $ip = validIp ($ip) ? $ip : '0.0.0.0';
+  }
+}
+if (!function_exists ('request_is_ajax')) {
+  function request_is_ajax () {
+    return isset ($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower ($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+  }
+}
+
+if (!request_is_ajax ()) {
+  header ('HTTP/1.1 500 Internal Server Error', true, 500);
+  echo "<title>500 Internal Server Error</title><body>500 Internal Server Error.Not Ajax.</body>";
+  exit;
+}
+
+$allow = array ('dev.case.ioa.tw', 'www.curatech.jp');
+isset ($_SERVER['HTTP_ORIGIN']) && in_array ($_SERVER['HTTP_ORIGIN'], $allow) || $_SERVER['HTTP_ORIGIN'] = 'dev.case.ioa.tw';
+// if (!(isset ($_SERVER['HTTP_ORIGIN']) && in_array ($_SERVER['HTTP_ORIGIN'], $allow))) {
+//   header ('HTTP/1.1 500 Internal Server Error', true, 500);
+//   echo "<title>500 Internal Server Error</title><body>500 Internal Server Error.Not Allow Domain.</body>";
+//   exit;
+// }
+
+$details = json_decode (file_get_contents ("http://ipinfo.io/" . ip ()), true);
+$country = isset ($details['country']) ? $details['country'] !== 'JP' ? $details['country'] === 'TW' ? 'tc' : 'en' : 'jp' : 'en';
+
+
+header ("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+header ('HTTP/1.1 200 OK', true, 200);
+header ('Content-Type: application/json; charset=UTF-8', true);
+echo json_encode (array ('country' => $country));
--- js/public.js
+++ js/public.js
@@ -4,7 +4,32 @@
  * @license     http://creativecommons.org/licenses/by-nc/2.0/tw/
  */
 
+function getStorage (key) { return (typeof Storage !== 'undefined') && (value = localStorage.getItem (key)) && (value = JSON.parse (value)) ? value : undefined; }
+function setStorage (key, data) { try { if (typeof Storage === 'undefined') return false; localStorage.setItem (key, JSON.stringify (data)); return true; } catch (err) { console.error ('設定 storage 失敗！', error); return false; } }
+
+window.storage = {};
+window.storage.country = {
+  cacheTime: 1000 * 60, //每分鐘
+  storageKey: 'cura.country',
+  set: function (val) { return setStorage (this.storageKey, { t: new Date ().getTime (), v: val }); },
+  get: function (d4) { var tmp = getStorage (this.storageKey); return (typeof tmp !== 'undefined' && typeof tmp.t !== 'undefined' && typeof tmp.v !== 'undefined' && tmp.t + this.cacheTime > new Date ().getTime () && (tmp.v === 'jp' || tmp.v === 'tc' || tmp.v === 'en')) ? tmp.v : (typeof d4 === 'undefined' ? '' : d4); },
+};
+
 $(function () {
+  $('#cholang select').each (function () {
+    $(this).change (function () {
+      var pathnames = location.pathname.split ('/').filter (function (t) {return t.length;});
+      var val = $(this).val ().replace (/^\//, "");
+      if (val.split ('/')[0] === 'jp' || val.split ('/')[0] === 'tc' || val.split ('/')[0] === 'en')
+        window.storage.country.set (val.split ('/')[0]);
+      else
+        window.storage.country.set ('en');
+
+      window.location.assign ((pathnames[0] === 'cura' ? '/cura' : '') + '/' + val);
+    });
+  });
+  
+
   if (typeof imgLiquid !== 'undefined') $('._ic').imgLiquid ({ verticalAlign:'center' });
 
   $('.banner_boxs').each (function () {
