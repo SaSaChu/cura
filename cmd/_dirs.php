e648de60e80342f090f96c69176dabe3370a5e85
OA Wu <comdan66@gmail.com>
2018-02-05 15:18:06 +0800

    修正錯誤與部署條件。

--- cmd/_dirs.php
+++ cmd/_dirs.php
@@ -13,7 +13,12 @@
 // 4. 是否包含隱藏檔
 $_dirs = array (
   ''     => [['html', 'txt'], false, false],
-  'js'   => [['js'], true, false],
+  ''     => [['html', 'txt'], false, false],
+  
+  'tc'   => [['html'], true, false],
+  'jp'   => [['html'], true, false],
+  'en'   => [['html'], true, false],
+
   'css'  => [['css'], true, false],
   'font' => [['eot', 'svg', 'ttf', 'woff'], true, false],
   'img'  => [['png', 'jpg', 'jpeg', 'gif', 'svg'], true, false],
